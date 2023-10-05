d1f71b836dbc36b7e1d4b4cd61ae8aac55b9255d
LipiHaq <142553260+LipiHaq@users.noreply.github.com>
2023-10-04 22:04:48 -0400

    Add files via upload

---
+++ Variables/A1_Declaring_Variables.java
@@ -0,0 +1,21 @@
+package A_Variables;
+
+	public class A1_Declaring_Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			String name = "lipi";
+			int age = 23;
+			int birthday = 1999;
+			String state = "New York";
+			String Subject = "CET";
+		
+		System.out.println("My name is "+name);
+		System.out.println("My age is "+age);
+		System.out.println("My birthday year is "+birthday);
+		System.out.println("I am live in "+state);
+		System.out.println("MySubject is "+Subject);
+
+}
+	}
+	
