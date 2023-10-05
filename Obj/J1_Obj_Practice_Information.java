bc595c2eeb6728cfdd0b1540a59a369650384589
LipiHaq <142553260+LipiHaq@users.noreply.github.com>
2023-10-04 22:17:15 -0400

    Add files via upload

---
+++ Obj/J1_Obj_Practice_Information.java
@@ -0,0 +1,13 @@
+package J_Obj;
+
+public class J1_Obj_Practice_Information {
+	
+	void greetings () {
+		 
+		 System.out.println("Hello, Everyone");
+	}
+	void Name (String n) {
+		 
+		System.out.println("My name is: " + n);
+	}
+}
---
+++ Obj/J2_Obj_Practice_Main.java
@@ -0,0 +1,13 @@
+package J_Obj;
+
+public class J2_Obj_Practice_Main {
+
+	public static void main(String[] args) {
+		J1_Obj_Practice_Information obj = new J1_Obj_Practice_Information();
+		obj.greetings();
+		obj.Name("Lipi");
+		
+
+	}
+
+}
