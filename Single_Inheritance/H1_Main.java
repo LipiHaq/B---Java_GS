a61d830729864f37267a23776dc7458da6a71ce7
LipiHaq <142553260+LipiHaq@users.noreply.github.com>
2023-10-04 22:13:28 -0400

    Add files via upload

---
+++ Single_Inheritance/H1_Main.java
@@ -0,0 +1,9 @@
+package H_Single_Inheritance;
+
+public class H1_Main {
+	int TotalStudents = 30;
+	int MaleStudents = 15;
+	int FemailStudents = 15;
+	
+
+}
---
+++ Single_Inheritance/H2_Student.java
@@ -0,0 +1,14 @@
+package H_Single_Inheritance;
+
+public class H2_Student extends H1_Main {
+
+	public static void main(String[] args) {
+		H2_Student obj = new H2_Student();
+		
+		System.out.println("Total students are: " + obj.TotalStudents);
+		System.out.println("Total Male students are: " + obj.MaleStudents);
+		System.out.println("Total femail studnets are: " + obj.FemailStudents);
+		
+	}
+
+}
